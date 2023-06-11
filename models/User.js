c779bd401e90201f2b4ae0874c19e7fe66359e11
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-10 17:42:08 -0700

    added reactionSchema

--- models/User.js
+++ models/User.js
@@ -27,3 +27,16 @@ const thoughtSchema = new mongoose.Schema({
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+    reactionBody: { type: { type: String, minlength: 1, maxlength: 280 }, required: true },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, get: function(timestamp) {
+        const date = new Date(timestamp);
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const year = date.getFullYear();
+        return `${month}/${day}/${year}`;
+    }}
+})
