1278a7352c8f7b650914c108855ff2aa3e9084b7
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-11 17:47:02 -0700

    created requests to get thoughts, get single thought, create thought, update thought, delete thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+// reactionSchema defines the shape of the subdocument
+const reactionSchema = new mongoose.Schema({
+    reactionId: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+    reactionBody: { type: { type: String, minlength: 1, maxlength: 280 }, required: true },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, get: function(timestamp) {
+        const date = new Date(timestamp);
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const year = date.getFullYear();
+        return `${month}/${day}/${year}`;
+    }}
+});
+
 // thoughtSchema defines the shape of the parent document
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: { type: String, minlength: 1, maxlength: 280 }, required: true },
@@ -16,20 +30,6 @@ const thoughtSchema = new mongoose.Schema({
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
-});
-
-// reactionSchema defines the shape of the subdocument
-const reactionSchema = new mongoose.Schema({
-    reactionId: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
-    reactionBody: { type: { type: String, minlength: 1, maxlength: 280 }, required: true },
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, get: function(timestamp) {
-        const date = new Date(timestamp);
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const year = date.getFullYear();
-        return `${month}/${day}/${year}`;
-    }}
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
