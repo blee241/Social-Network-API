cf33d8ce27af8d7d1cb6fb40b2196818ff524391
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-11 18:29:18 -0700

    added the rest of the routes for Thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -15,4 +15,10 @@ router.route('/').get(getThoughts).post(createThought)
 // /api/thought/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addThoughtReaction);
+
+// /api/thoughts/:thoughtId/reactions/reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
+
 module.exports = router;
