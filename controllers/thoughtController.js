9d9e05598494729115408c2f8880dcdec2c06191
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-11 17:50:01 -0700

    completed addThoughtReaction request

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -82,5 +82,22 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err)
         }
+    },
+    async addThoughtReaction (req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $addToSet: { reactions: req.body }},
+                { runValidators: true, new: true }
+            )
+
+            if (!thought) {
+                return res.status(404).json({message: 'No thought with that id'})
+            }
+
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err)
+        }
     }
 }
